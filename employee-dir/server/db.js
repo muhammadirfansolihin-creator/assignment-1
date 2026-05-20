71daa1a497568a32cbd27fdd2a2d00e717ea174d
Irfan <muhammadirfansolihin@graduate.utm.my>
2026-05-20 23:35:38 +0800

    Creating the connection pool

---
+++ employee-dir/server/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise')
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_dir',
+    waitForConnections: true,
+    connectionLimit:10,
+    queueLimit:0
+})
